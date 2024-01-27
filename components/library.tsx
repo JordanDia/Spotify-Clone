fd5f8293fcbf727578b5f3a0e6268277d52beea7
Jordan Dia <jordandia101@gmail.com>
2024-01-27 14:25:57 -0500

    Users can upload songs locally. Adds the image and song files to the database.

--- components/library.tsx
+++ components/library.tsx
@@ -2,10 +2,23 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai"
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
     const onClick = () => {
-        // Handle upload later
+        // If not logged in, open login modal
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+        // check for subscriptions
+
+        return uploadModal.onOpen();
     };
 
     return (
